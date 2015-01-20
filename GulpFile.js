a78e74bd0afbfe0161568cf21c9db2efd4069b7e
Mark Moissette <kaosat.dev@gmail.com>
2015-01-20 15:34:06 +0100

    - updated gulpfile for better build process  * improved overall structure  * added tasks for copying & vulcanizing  * added task to build full node-webkit app from sources  * various cleanups  * updated package dependencies

--- GulpFile.js
+++ GulpFile.js
@@ -1,12 +1,24 @@
-var NwBuilder = require('node-webkit-builder');
 var gulp = require('gulp');
 var gutil = require('gulp-util');
+var shell = require('gulp-shell');
+var runSequence = require('run-sequence');
+
+var NwBuilder = require('node-webkit-builder');
+var vulcanize = require('gulp-vulcanize');
+
+var SRC_DIR  = 'src';
+var DEST_DIR = 'app';
+
+//which node webkit version to use
+var nwVersion  = '0.11.5';
+//which platform to use? (TODO:not sure how to deal with this
+var nwPlatform = 'linux64';
 
 gulp.task('nw', function () {
 
     var nw = new NwBuilder({
-        version: '0.11.5',
-        files: './app/**',
+        version: nwVersion,
+        files: DEST_DIR+"/**",/*'./app/**',*/
         /*macIcns: './icons/icon.icns',
         macPlist: {mac_bundle_id: 'myPkg'},*/
         platforms: ['linux64']
@@ -23,4 +35,43 @@ gulp.task('nw', function () {
     });
 });
 
-gulp.task('default', ['nw']);
+gulp.task('vulcanize', function () {
+    return gulp.src('src/usco-desktop.html')
+        .pipe(vulcanize({
+            dest: DEST_DIR,
+            strip: true,
+            csp:true,
+            inline:true
+        }))
+        .pipe(gulp.dest(DEST_DIR));
+});
+
+/*copy any needed files to app folder*/
+gulp.task('copy', function(){
+    
+    return gulp.src([SRC_DIR+'/index.html',SRC_DIR+'/webcomponents.min.js','package.json'])
+    .pipe(gulp.dest('app'));
+});
+
+/*build node webkit version of the app*/
+gulp.task('build:nw',  function(callback){
+  runSequence(["copy","vulcanize"], 'nw',
+              callback);
+});
+
+/* do not regenerate a final node webkit app, just launch it 
+(for dev purposes) */
+gulp.task('testrun:nw',  function(callback){
+  var nwPath = "./cache/"+nwVersion+"/"+nwPlatform+"/nw "+DEST_DIR+"/index.html"
+  gutil.log('nw exec path', nwPath);
+  //var cmd = 
+  /*var cmd = shell( nwPath );
+  cmd.on('error', function (msg) {
+        gutil.log('nw error', msg);
+  });*/
+  return gulp.src('').pipe(
+    shell( [nwPath] ) );
+  //return shell.task( [nwPath] );//cmd();
+});
+
+gulp.task('default', ['vulcanize', 'copy', 'nw']);
